322bb8d3de99d6537995150e7a817db70ec7dbeb
Jonathan Beezley <jonathan.beezley@kitware.com>
2015-06-19 13:40:10 -0400

    Make my own travis_wait

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@ set -e -x
 mkdir "${buildpath}"
 pushd "${buildpath}"
 cmake ${cmake_opts} "-DCMAKE_INSTALL_PREFIX=$HOME/$prefix" "../$srcpath"
-make
+make &
+while ps -p $! >&-; do
+    sleep 60
+    echo "Ping!"
+done
 popd
